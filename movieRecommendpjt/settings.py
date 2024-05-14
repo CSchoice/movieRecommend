70cb8842617b208c7fb5fc5e441de3672f0ffc7a
CSchoice <tkdlqjf@gmail.com>
2024-05-14 10:14:07 +0900

    Docs: settings.py 주석 수정

--- movieRecommendpjt/settings.py
+++ movieRecommendpjt/settings.py
@@ -149,11 +149,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
-    # Authentication
+    # 인증절차
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    # permission
+    # 자격증명
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.AllowAny',
     # ],
