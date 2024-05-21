40dcbbcda3dae516cdfdcb62f2a159f2437e5d51
CSchoice <tkdlqjf@gmail.com>
2024-05-21 17:21:17 +0900

    Fix: chatgpt 파일 수정

--- chatgpt_chatbot.py
+++ chatgpt_chatbot.py
@@ -2,12 +2,12 @@ import openai
 from decouple import config
 
 # API 키 설정
-OPENAI_API_KEY = config('GPT_KEY')
+openai.api_key = config('GPT_KEY')
 TMDB_API_KEY = '970ff4106d7c75d8a8b06078e351280f'
 
 def get_movie_recommendation(messages):
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-4o",
         messages=messages,
         max_tokens=700
     )
@@ -15,16 +15,18 @@ def get_movie_recommendation(messages):
 
 # 챗봇 루프
 def chatbot():
-    print("I will recommend a movie based on your current mood. Type 'exit' to end the conversation.")
+    print("현재 기분에 따라 영화를 추천해 드립니다. 'exit'을 입력하면 대화가 종료됩니다.")
     
     messages = [
-        {"role": "system", "content": "You are an expert in recommending movies and Korean."}
+        {"role": "system", "content": "You are an expert in recommending movies and Korean."},
+        {"role": "user", "content": '사용자가 입력한 감정을 해소할 수 있는 장르를 3개 추천해주고, 그 장르를 바탕으로 영화를 10개 추천해줘, 그리고 영화 포스터 이미지를 구글에서 찾아서 보여줘'},
+        {"role": "user", "content": '그 영화들의 포스터 이미지를 같이 보내줘'},
     ]
     
     while True:
-        user_input = input("How are you feeling right now? (e.g., I feel sad): ")
+        user_input = input("지금 기분이 어떠신가요? (예: 기분이 우울해요): ")
         if user_input.lower() == "exit":
-            print("Ending the chatbot. Have a great day!")
+            print("챗봇을 종료합니다. 좋은 하루 되세요!")
             break
         
         # 사용자 입력을 messages 리스트에 추가
@@ -37,7 +39,7 @@ def chatbot():
         messages.append({"role": "assistant", "content": recommendation})
         
         # 추천 영화 출력
-        print(f"Recommended movie: {recommendation}")
+        print(f"추천 영화: {recommendation}")
 
 # 챗봇 실행
 chatbot()
